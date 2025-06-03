40a3774347c472209c2d9d00678389b67387de60
Gustavo Bressan <gus7avo182@gmail.com>
2025-06-02 22:36:13 -0300

    update: Separando por funções

---
+++ src/automation/automation.py
@@ -0,0 +1,36 @@
+class Automation:
+    
+    def __init__(self, url: str, driver = None):
+    
+        self.driver = driver.get(url) if driver else None
+        self.url = url
+        print("==============================")
+        print("Inicializando a automação...")
+        print("==============================\n")
+
+    async def get_title(self, driver):
+        print("Obtendo o título da página...\n")
+        return driver.title
+    
+    async def get_url(self, driver):
+        print("Obtendo a URL da página...\n")
+        return driver.current_url
+    
+    async def get_element_by_css(self, driver, selector):
+        print(f"Obtendo o elemento pelo seletor CSS: {selector}")
+        try:
+            element = driver.find_element("css selector", selector)
+            return element.text
+        except Exception as e:
+            print(f"Erro ao obter o elemento: {e}\n")
+            return e
+
+    async def get_element_by_xpath(self, driver, xpath):
+        print(f"Obtendo o elemento pelo XPath: {xpath}\n")
+        try:
+            element = driver.find_element("xpath", xpath)
+            return element.text
+        except Exception as e:
+            print(f"Erro ao obter o elemento: {e}\n")
+            return e
+
--- src/main.py
+++ src/main.py
@@ -4,28 +4,60 @@ from selenium.webdriver.chrome.service import Service # type: ignore
 from webdriver_manager.chrome import ChromeDriverManager # type: ignore
 from selenium.webdriver.common.by import By # type: ignore
 
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/114.0.5735.90 Safari/537.36")
+import sys
+import asyncio
 
-# - O WebDriver Manager baixa e instala a versão correta do ChromeDriver, evitando problemas de compatibilidade com o Google Chrome instalado.
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-driver.get("https://www.magazineluiza.com.br/celular-samsung-galaxy-s25-5g-128gb-12gb-ram-camera-tripla-de-50-12-10-tela-grande-de-6-2/p/gehgkef1jc/te/gs25/")
+from automation.automation import Automation
 
-print(driver.title)
-print(driver.current_url)
+async def main():
 
-print(driver.find_element("xpath", "//p[contains(@class, 'sc-dcJsrY') and contains(@class, 'eLxcFM') and contains(@class, 'sc-hgRRfv') and contains(@class, 'dfAhbD')]").text)
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/114.0.5735.90 Safari/537.36")
 
-# print(driver.page_source)
-# print(driver.find_element("css selector", ".h3.mb-3.selenium-ide").text)
+    # - O WebDriver Manager baixa e instala a versão correta do ChromeDriver, evitando problemas de compatibilidade com o Google Chrome instalado.
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-# # Seleciona o elemento com a classe h3, mb-3 e selenium-ide, restringindo a busca ao elemento <h4> que contém essas classes.
-# print(driver.find_element("xpath", "//h4[contains(@class, 'h3') and contains(@class, 'mb-3') and contains(@class, 'selenium-ide')]").text)
+    # Url desejada para automação
+    url = "https://www.magazineluiza.com.br/celular-samsung-galaxy-s25-5g-128gb-12gb-ram-camera-tripla-de-50-12-10-tela-grande-de-6-2/p/gehgkef1jc/te/gs25/"
 
-driver.close()
+    # - Inicializa a automação com a URL desejada
+    automation = Automation(url = url, driver = driver)
+
+    # driver.get(url)
+
+    # print(driver.find_element("xpath", "//p[contains(@class, 'sc-dcJsrY') and contains(@class, 'eLxcFM') and contains(@class, 'sc-hgRRfv') and contains(@class, 'dfAhbD')]").text)
+
+    # Retorna o título da página
+    responde = await automation.get_title(driver)
+    print("==============================")
+    print(responde)
+    print("==============================\n")
+
+    #retorna a URL da página
+    responde = await automation.get_url(driver)
+    print("==============================")
+    print(responde)
+    print("==============================\n")
+
+    # Retorna o elemento pelo seletor CSS
+    responde = await automation.get_element_by_css(driver, selector = "h1")
+    print("==============================")
+    print(responde)
+    print("==============================\n")
+
+    # Retorna o elemento pelo XPath
+    responde = await automation.get_element_by_xpath(driver, xpath = "//p[contains(@class, 'sc-dcJsrY') and contains(@class, 'eLxcFM') and contains(@class, 'sc-hgRRfv') and contains(@class, 'dfAhbD')]")
+    print("==============================")
+    print(responde)
+    print("==============================\n")
+
+
+    driver.close()
+
+asyncio.run(main())
 
 
