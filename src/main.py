fccb394ad762c0a3f938d612701f90a277bb1f3b
Gustavo Bressan <gus7avo182@gmail.com>
2025-06-04 00:27:58 -0300

    update: iniciando uma função que recebe uma lista de links e retorna uma lista de preços

--- src/main.py
+++ src/main.py
@@ -8,9 +8,14 @@ import asyncio
 
 from automation.automation  import Automation
 from drivers.driver         import Driver
+from prices.prices          import Prices
 
 async def main():
+    print("Iniciando a aplicação...\n")
 
+    await run_prices()
+
+async def tests():
     # Url desejada para automação
     url = "https://www.magazineluiza.com.br/celular-samsung-galaxy-s25-5g-128gb-12gb-ram-camera-tripla-de-50-12-10-tela-grande-de-6-2/p/gehgkef1jc/te/gs25/"
 
@@ -44,6 +49,18 @@ async def main():
     finally:
         # Fechando o driver
         await driver.close_driver()
+
+async def run_prices():
+
+    prices = Prices(links = [
+        "https://www.magazineluiza.com.br/celular-samsung-galaxy-s25-5g-128gb-12gb-ram-camera-tripla-de-50-12-10-tela-grande-de-6-2/p/gehgkef1jc/te/gs25/",
+        "https://www.magazineluiza.com.br/apple-iphone-16e-128gb-branco-61-48mp-ios-5g/p/240059700/te/i16e/",
+        "https://www.magazineluiza.com.br/apple-iphone-14-128gb-estelar-61-12mp-ios-5g/p/237184100/te/ip14/"
+    ])
+
+    prices_response = await prices.get_prices(links = prices.links)
+    print(prices_response)
+
 
 
 asyncio.run(main())
---
+++ src/prices/prices.py
@@ -0,0 +1,43 @@
+# Essa classe é responsavel por receber uma lista de links para buscar os preços dos produtos
+
+from drivers.driver import Driver
+from automation.automation import Automation
+
+class Prices:
+
+    def __init__(self, links: list[str]):
+
+        self.links = links
+
+    async def get_prices(self, links: list[str]) -> list[str]:
+
+        driver = Driver()
+        responses = []
+
+        for link in links:
+
+            drivers = await driver.init_driver(url = link)
+            automation = Automation(url = link, driver = drivers)
+
+            try:
+                # response = await automation.get_title(drivers)
+                # print(f'Título da página: {response}\n')
+
+                # Retorna o elemento pelo XPath
+                responde = await automation.get_element_by_xpath(drivers, xpath = "//p[contains(@class, 'sc-dcJsrY') and contains(@class, 'eLxcFM') and contains(@class, 'sc-hgRRfv') and contains(@class, 'dfAhbD')]")
+                print(f'{responde}\n')
+
+                responses.append(responde)
+
+            except Exception as e:
+                print(f"Erro ao inicializar a automação: {e}")
+        
+        await driver.close_driver()
+
+        return responses
+
+
+
+
+
+    
